d4aeaef1ee408de7f08d96b080f0fab7569f9b39
ajayagrawal05 <abkbalways@gmail.com>
2023-01-13 11:36:38 +0530

    First Commit

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,17 @@
+class EmailsController < ApplicationController
+
+    def index
+    end
+
+    def new
+      @email = []
+    end
+
+    def ceate
+    end
+
+    private
+    def email_params
+    end
+
+end
