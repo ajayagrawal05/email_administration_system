e3dc3d4c75993de88e3a55e89b4d3acee51c9f2a
ajayagrawal05 <abkbalways@gmail.com>
2023-01-14 20:22:08 +0530

    Add Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_171724) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_144412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_171724) do
     t.integer "interval_between"
     t.integer "frequency"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
