c73e1a5966f0814df9fd6ac1088f028dbfb3f220
ajayagrawal05 <abkbalways@gmail.com>
2023-01-16 23:24:11 +0530

    Fix UI/UX

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$('#email_email_type').change(function(){
+	if($('#email_email_type').val() == 'Transaction'){
+		$('#reminder_params').hide();
+	}
+	else {
+		$('#reminder_params').show();
+	}
+});
